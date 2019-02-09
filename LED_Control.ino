77920be4e33dfdbd840ee978200ef4ea570f0767
Mohamed khalil <khalil.kahlaoui@gmail.com>
2019-02-09 12:04:57 +0100

    Update LED_Control.ino

--- LED_Control.ino
+++ LED_Control.ino
@@ -1,20 +1,25 @@
 void setup() {
-Serial.begin(9600);
- pinMode(8, OUTPUT); // put your setup code here, to run once:
- }
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
- if(Serial.available()>0)
-   {     
-      char data= Serial.read(); // reading the data received from the bluetooth module
-      switch(data)
-      {
-        case 'a': digitalWrite(8, HIGH);break; // when a is pressed on the app on your smart phone
-        case 'd': digitalWrite(8, LOW);break; // when d is pressed on the app on your smart phone
-        default : break;
-      }
-      Serial.println(data);
-   }
-   delay(50);
+ 
+  if (Serial.available() > 0)
+  {
+    char data = Serial.read(); 
+    switch (data)
+    {
+      case '1':
+        digitalWrite(13, HIGH);
+        break; 
+      case '2':
+        digitalWrite(13, LOW);
+        break; 
+      default :
+      break;
+    }
+    Serial.println(data);
+  }
+  delay(50);
 }
