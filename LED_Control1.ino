6c9e25b859ef6853b44b5293e8ab2f1a8ac542b3
Mohamed khalil <khalil.kahlaoui@gmail.com>
2019-02-09 15:30:26 +0100

    Update LED_Control1.ino

--- LED_Control1.ino
+++ LED_Control1.ino
@@ -2,18 +2,20 @@ int state = 0;
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  Serial.begin(38400); 
+  Serial.begin(9600);
 }
 void loop() {
-  if(Serial.available() > 0){
-    state = Serial.read(); 
- }
- if (state == '0') {
-  digitalWrite(13, LOW);
-  state = 0;
- }
- else if (state == '1') {
-  digitalWrite(13, HIGH);
-  state = 0;
- } 
+  if (Serial.available() > 0) {
+    state = Serial.read();
+  }
+  if (state == '0') {
+    digitalWrite(13, LOW);
+    state = 0;
+  }
+  else if (state == '1') {
+    digitalWrite(13, HIGH);
+    delay(100);
+    state = 0;
+  }
+ Serial.println(state);
 }
